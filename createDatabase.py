a0303774de9e5255e09fe87a190248a406f316e6
Varun Kharkwal <varunkh12345@gmail.com>
2024-04-28 02:47:16 +0530

    introduced mediapipe to plot points and line on hands in graph + point cordinates

--- createDatabase.py
+++ createDatabase.py
@@ -1,6 +1,13 @@
 import os
 import cv2
+import mediapipe as mp
 import matplotlib.pyplot as plt
+
+mp_hands = mp.solutions.hands
+mp_drawing = mp.solutions.drawing_utils
+mp_drawing_styles = mp.solutions.drawing_styles
+
+hands = mp_hands.Hands(static_image_mode=True, min_detection_confidence=0.3)
 
 DATA_DIR = './data'
 
@@ -10,6 +17,19 @@ for dir_ in os.listdir(DATA_DIR):
     print('Image: {}'.format(img_path))
     img = cv2.imread(os.path.join(DATA_DIR, dir_, img_path))
     img_RGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+    results = hands.process(img_RGB)
+    for hand_landmarks in results.multi_hand_landmarks:
+      mp_drawing.draw_landmarks(
+        img_RGB, # image to draw
+        hand_landmarks, # model output
+        mp_hands.HAND_CONNECTIONS, # hand connections
+        mp_drawing_styles.get_default_hand_landmarks_style(), # drawing styles
+        mp_drawing_styles.get_default_hand_connections_style() # drawing styles
+      )
+      for i in range(len(hand_landmarks.landmark)):
+        print(i, hand_landmarks.landmark[i])
+
     plt.figure()
     plt.imshow(img_RGB)
 plt.show()
